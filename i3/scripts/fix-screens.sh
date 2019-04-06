5629d321b66594cf85484c5e95020a7bf6645dac
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2019-04-06 23:08:08 +0200

    Updated compton and changed background scaling mode

--- i3/scripts/fix-screens.sh
+++ i3/scripts/fix-screens.sh
@@ -3,4 +3,4 @@
 BACKGROUNDPATH=$(awk '/\*backgroundPath:(.*)/ { print $2 }' < ~/.Xresources)
 
 #Apply xrandr config and set bg. Using && to make the wallpaper command wait
-/home/jlndk/.screenlayout/main.sh && feh --bg-scale $BACKGROUNDPATH
+/home/jlndk/.screenlayout/main.sh && feh --bg-max $BACKGROUNDPATH
